9bc68df3eba6d63db85076c45ea406040b2f7c0b
muri romagnoli <muriroma@gmail.com>
2022-08-30 16:57:10 -0300

    fetch en tu proyecto

--- js/carrito.js
+++ js/carrito.js
@@ -1,86 +1,161 @@
 let carrito = cargarCarrito();
 
-let sectionProductos = document.getElementById("section-productos");
-let sectionCarrito = document.getElementById("section-carrito");
+let productosJSON = [];
 
-let totalCompra = document.createElement("div");
-totalCompra.innerHTML = "<h2>Total: $</h2>";
-sectionCarrito.appendChild(totalCompra);
+let cantidadTotalCompra = carrito.length;
 
-let montoTotalCompra = document.createElement("h2");
-montoTotalCompra.innerText = "0";
-totalCompra.appendChild(montoTotalCompra);
+$(document).ready(function () {
+  $("#cantidad-compra").text(cantidadTotalCompra);
 
-let cantidadProductos = document.createElement("div");
-cantidadProductos.innerHTML = "<h3>Cantidad de productos: </h3>";
-sectionCarrito.appendChild(cantidadProductos);
+  $("#btn-finalizar").on("click", function () {
+    Swal.fire({
+      title: "¿Seguro que queres finalizar tu compra?",
+      text: `Total a abonar: $${calcularTotalCarrito()}`,
+      showCancelButton: true,
+      confirmButtonColor: "#008f39",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si",
+      cancelButtonText: "No",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Compra confirmada", "¡Que lo disfrutes!", "success");
+        vaciarCarrito();
+      }
+    });
+  });
 
-let cantProductos = document.createElement("h3");
-cantProductos.innerText = "0";
-cantidadProductos.appendChild(cantProductos);
+  $("#seleccion option[value='pordefecto']").attr("selected", true);
+  $("#seleccion").on("change", ordenarProductos);
 
-let botonFinalizar = document.createElement("button");
-botonFinalizar.innerText = "Finalizar compra";
-sectionCarrito.appendChild(botonFinalizar);
-botonFinalizar.setAttribute("class", "boton");
+  $("#gastoTotal").html(`Total: $ ${calcularTotalCarrito()}`);
+  obtenerJSON();
+  renderizarProductos();
+  mostrarEnTabla();
+});
 
+function renderizarProductos() {
+  for (const producto of productosJSON) {
+    $("#section-productos").append(`<div class="card-product"> 
+                                    <div class="img-container">
+                                    <img src="${producto.foto}" alt="${producto.nombre}" class="img-product"/>
+                                    </div>
+                                    <div class="info-producto">
+                                    <p class="font">${producto.nombre}</p>
+                                    <strong class="font">$${producto.precio}</strong>
+                                    <button class="botones" id="btn${producto.id}"> Agregar al carrito </button>
+                                    </div>
+                                    </div>`);
 
-botonFinalizar.onclick = () => {
-  const precioFinal = montoTotalCompra.innerText;
-  Swal.fire({
-    title: '¿Finalizar compra?',
-    text: `Monto total: $${precioFinal}`,
-    showCancelButton: true,
-    confirmButtonColor: '#008f39',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Si',
-    cancelButtonText: 'No'
-  }).then((result) => {
-    if (result.isConfirmed) {
-      Swal.fire(
-        'Compra confirmada',
-        '¡Que lo disfrutes!',
-        'success'
-      )
-      vaciarCarrito();
+    $(`#btn${producto.id}`).on("click", function () {
+      agregarAlCarrito(producto);
+      $(`#btn${producto.id}`).fadeOut(200).fadeIn(200);
+    });
+  }
+}
+
+function obtenerJSON() {
+  $.getJSON("./json/productos.json", function (respuesta, estado) {
+    if (estado == "success") {
+      productosJSON = respuesta;
+      renderizarProductos();
     }
-  })
+  });
 }
 
-for (const producto of productos) {
-  let container = document.createElement("div");
-  container.setAttribute("class", "card-product");
-  container.innerHTML = ` <div class="img-container">
-                            <img src="${producto.foto}" alt="${producto.nombre}" class="img-product"/>
-                            </div>
-                            <div class="info-producto">
-                            <p class="font">${producto.nombre}</p>
-                            <strong class="font">$${producto.precio}</strong>
-                            <button class="boton" id="btn${producto.id}"> Agregar al carrito </button>
-                            </div>`;
-  sectionProductos.appendChild(container);
-  document.getElementById(`btn${producto.id}`).onclick = () => agregarAlCarrito(`${producto.id}`);
+function ordenarProductos() {
+  let seleccion = $("#seleccion").val();
+  if (seleccion == "menor") {
+    productosJSON.sort(function (a, b) {
+      return a.precio - b.precio;
+    });
+  } else if (seleccion == "mayor") {
+    productosJSON.sort(function (a, b) {
+      return b.precio - a.precio;
+    });
+  } else if (seleccion == "alfabetico") {
+    productosJSON.sort(function (a, b) {
+      return a.nombre.localeCompare(b.nombre);
+    });
+  }
+
+  $(".card-product").remove();
+  renderizarProductos();
 }
 
+class ProductoCarrito {
+  constructor(prod) {
+    this.id = prod.id;
+    this.foto = prod.foto;
+    this.nombre = prod.nombre;
+    this.precio = prod.precio;
+    this.cantidad = 1;
+  }
+}
 
-function agregarAlCarrito(id) {
-  carrito.push(productos.find(p => p.id == id));
+function agregarAlCarrito(productoAgregado) {
+  let encontrado = carrito.find((p) => p.id == productoAgregado.id);
+  if (encontrado == undefined) {
+    let productoEnCarrito = new ProductoCarrito(productoAgregado);
+    carrito.push(productoEnCarrito);
+    Swal.fire(
+      "Nuevo producto agregado al carrito",
+      productoAgregado.nombre,
+      "success"
+    );
+
+    $("#tablabody")
+      .append(`<tr id='fila${productoEnCarrito.id}' class='tabla-carrito'>
+                            <td> ${productoEnCarrito.nombre}</td>
+                            <td id='${productoEnCarrito.id}'> ${productoEnCarrito.cantidad}</td>
+                            <td> ${productoEnCarrito.precio}</td>
+                            <td><button class='btn btn-light' id="btn-eliminar-${productoEnCarrito.id}">🗑️</button></td>
+                            </tr>`);
+  } else {
+    let posicion = carrito.findIndex((p) => p.id == productoAgregado.id);
+    carrito[posicion].cantidad += 1;
+    $(`#${productoAgregado.id}`).html(carrito[posicion].cantidad);
+  }
+
+  $("#gastoTotal").html(`Total: $ ${calcularTotalCarrito()}`);
   localStorage.setItem("carrito", JSON.stringify(carrito));
-  calcularTotalCarrito();
+  mostrarEnTabla();
+}
+
+function mostrarEnTabla() {
+  $("#tablabody").empty();
+  for (const prod of carrito) {
+    $("#tablabody").append(`<tr id='fila${prod.id}' class='tabla-carrito'>
+                            <td> ${prod.nombre}</td>
+                            <td id='${prod.id}'> ${prod.cantidad}</td>
+                            <td> ${prod.precio}</td>
+                            <td><button class='btn btn-light' id="eliminar${prod.id}">🗑️</button></td>
+                            </tr>`);
+
+    $(`#eliminar${prod.id}`).click(function () {
+      let eliminado = carrito.findIndex((p) => p.id == prod.id);
+      carrito.splice(eliminado, 1);
+      console.log(eliminado);
+      $(`#fila${prod.id}`).remove();
+      $("#gastoTotal").html(`Total: $ ${calcularTotalCarrito()}`);
+      localStorage.setItem("carrito", JSON.stringify(carrito));
+    });
+  }
 }
 
 function calcularTotalCarrito() {
   let total = 0;
   for (const producto of carrito) {
-    total += producto.precio;
+    total += producto.precio * producto.cantidad;
   }
-  montoTotalCompra.innerText = total;
-  cantProductos.innerText = carrito.length;
+  $("#montoTotalCompra").text(total);
+  $("#cantidad-compra").text(carrito.length);
+  return total;
 }
 
 function vaciarCarrito() {
-  montoTotalCompra.innerText = "0";
-  cantProductos.innerText = "0";
+  $("#gastoTotal").text("Total: $0");
+  $("#cantidad-compra").text("0");
+  $(".tabla-carrito").remove();
   localStorage.clear();
   carrito = [];
 }
